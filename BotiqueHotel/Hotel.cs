629ecfb77b73baf73b27a362b55e56ec3d5c7de8
aaron <arnquest@hotmail.com>
2022-05-02 23:20:14 +0800

    Add project files.

---
+++ BotiqueHotel/Hotel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BotiqueHotel
+{
+    internal class Hotel
+    {
+        private Room[][] rooms;
+       
+        public Hotel()
+        {
+
+        }
+        public void printRooms()
+        {
+
+        }
+        public bool AssignRoom(int roomnumber)
+        {
+            return true;
+        }
+        public bool MarkRepair(int roomNo)
+        {
+            return true;
+        }
+        public bool MarkCleaned(int roomNo)
+        {
+            return true;
+        }
+        public bool CheckOut(int roomNo)
+        {
+            return true;
+        }
+        public List<string> GetAvailableRooms()
+        { 
+            List<string> availableRooms = new List<string>();
+
+            return availableRooms;
+        }
+    }
+}
---
+++ BotiqueHotel/Room.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BotiqueHotel
+{
+    public class Room
+    {
+        private int roomnumber;
+        private String type;
+        private bool flag;
+
+        public Room()
+        {
+        }
+
+        public Room(int roomnumber, String type, bool flag)
+        {
+            this.roomnumber = roomnumber;
+            this.type = type;
+            this.flag = flag;
+        }
+
+        public int getRoomnumber()
+        {
+            return roomnumber;
+        }
+
+        public void setRoomnumber(int roomnumber)
+        {
+            this.roomnumber = roomnumber;
+        }
+
+        public String getType()
+        {
+            return type;
+        }
+
+        public void setType(String type)
+        {
+            this.type = type;
+        }
+
+        public bool isFlag()
+        {
+            return flag;
+        }
+
+        public void setFlag(bool flag)
+        {
+            this.flag = flag;
+        }
+  
+        //Override the toString method, which can be converted to output room information
+        public String toString()
+        {
+            //ternary operator
+            return "[" + this.getRoomnumber() + "," + this.getType() + "," + (this.isFlag() ? "Occupy" : "free") + "]";
+        }
+    }
+}
